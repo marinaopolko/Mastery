479d46ccdf90c8aebf536d056f517ad647cf29a3
Marina Opolko <marina.opolko@gmail.com>
2019-12-16 10:39:33 +0300

    Update project.js

--- project.js
+++ project.js
@@ -6,11 +6,12 @@ Array.prototype.shuffle = function() {
 var td = document.getElementsByTagName('td');
 var color = ["red", "cadetblue", "firebrick", "lightblue", "orchid", "floralwhite", "lavenderblush", "lightgrey", "blue"];
 
-window.onload = function{}
-for( var i=0; i<td.length; i++){
-    var color = ["red", "cadetblue", "firebrick", "lightblue", "orchid", "floralwhite", "lavenderblush", "lightgrey", "blue"].shuffle();
-    for( var j = 0; j<color.length; j++ ){
-        td[i].style.backgroundColor = color[j];
+window.onload = function(){
+    for (var i = 0; i < td.length; i++) {
+        var color = ["red", "cadetblue", "firebrick", "lightblue", "orchid", "floralwhite", "lavenderblush", "lightgrey", "blue"].shuffle();
+        for (var j = 0; j < color.length; j++) {
+            td[i].style.backgroundColor = color[j];
+        }
     }
 }
 
