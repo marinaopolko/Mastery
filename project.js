f6ae2253dd932acf13d03a2da3e2c3e205308216
Marina Opolko <marina.opolko@gmail.com>
2019-12-16 11:20:48 +0300

    Changes

--- project.js
+++ project.js
@@ -5,8 +5,21 @@ Array.prototype.shuffle = function() {
 };
 var td = document.getElementsByTagName('td');
 var color = ["red", "cadetblue", "firebrick", "lightblue", "orchid", "floralwhite", "lavenderblush", "lightgrey", "blue"];
+var audio = document.getElementsByTagName('audio');
+document.addEventListener('DOMContentLoaded',colorMusic);
 
-window.onload = function(){
+function colorMusic(){
+    audio.play();
+    if (audio.volume = 0.1) {
+        setInterval(tdBackground, 3000);
+    } else if (audio.volume = 0.5){
+        setInterval(tdBackground, 2000);
+    } else {
+        setInterval(tdBackground, 1000);
+    }
+
+}
+function tdBackground(){
     for (var i = 0; i < td.length; i++) {
         var color = ["red", "cadetblue", "firebrick", "lightblue", "orchid", "floralwhite", "lavenderblush", "lightgrey", "blue"].shuffle();
         for (var j = 0; j < color.length; j++) {
